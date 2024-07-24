b427317f543718db7941b410a33b45b21e453099
Enver GÖKMEN <envergokmen@gmail.com>
2024-07-24 13:18:53 +0300

    BE-4093 env name convention applied

--- main.sh
+++ main.sh
@@ -4,19 +4,19 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 locale
 
-curl -o "./$AndroidFileName" -k $AndroidFileUrl
+curl -o "./$AC_APP_FILE_NAME" -k $AC_APP_FILE_URL
 
 bundle init
-mv "$Gemfile" "Gemfile"
+mv "$AC_GEM_FILE" "Gemfile"
 bundle install
-mv "$Pluginfile" "Pluginfile"
+mv "$AC_PLUGIN_FILE" "Pluginfile"
 mkdir fastlane
 touch fastlane/Fastfile
-mv "$FastFileConfig" "fastlane/Fastfile"
-mv "$ApiKey" "$ApiKeyFileName"
+mv "$AC_FASTFILE_CONFIG" "fastlane/Fastfile"
+mv "$AC_API_KEY" "$AC_API_KEY_FILE_NAME"
 bundle exec fastlane add_plugin huawei_appgallery_connect
 bundle exec fastlane install_plugins
-bundle exec fastlane $FastlaneParams --verbose
+bundle exec fastlane $AC_FASTLANE_PARAMS --verbose
 if [ $? -eq 0 ]
 then
 echo "AppGallery progress succeeded"
